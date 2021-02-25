d4c2f2c311d6bacd4a50f89ad7622491974285e2
vsahinid <vsahinid@alumni.cmu.edu>
2021-02-24 20:20:37 -0600

    Added playercontroller, api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/players', 'PlayerController@index');
+Route::get('/players/{id}', 'PlayerController@show');
+Route::post('/players', 'PlayerController@store');
+Route::post('/players/{id}/answers', 'PlayerController@answer');
+Route::delete('/players/{id}', 'PlayerController@delete');
+Route::delete('/players/{id}/answers', 'PlayerController@resetAnswers');
